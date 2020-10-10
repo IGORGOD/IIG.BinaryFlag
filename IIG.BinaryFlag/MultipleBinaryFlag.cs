9e51234f524c5d3fb93b507d67cf745c6b219911
Ihor Babarykin <kittoryu@gmail.com>
2020-10-10 18:16:57 +0300

    Initial commit

---
+++ IIG.BinaryFlag/MultipleBinaryFlag.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace IIG.BinaryFlag
+{
+    /// <summary>
+    ///     Class that Provides Functional for Creating and Working with Multiple Binary Flag Object
+    /// </summary>
+    public class MultipleBinaryFlag : IDisposable
+    {
+        private readonly ConcreteBinaryFlag _concreteFlag;
+        protected readonly SafeHandle Handle = new SafeFileHandle(IntPtr.Zero, true);
+        protected bool Disposed;
+
+        /// <summary>
+        ///     Constructor for Multiple Binary Flag Object
+        /// </summary>
+        /// <param name="length">Length of Binary Flag - How Many Binaries Does Multiple Binary Flag Contain </param>
+        /// <param name="initialValue">Initial Value of Binary Elements of Flag</param>
+        public MultipleBinaryFlag(ulong length, bool initialValue = true)
+        {
+            if (length < 2)
+                throw new ArgumentOutOfRangeException("Length of Flag must be bigger than one");
+            if (length > 17179868704)
+                throw new ArgumentOutOfRangeException("Length of Flag must be lesser than '17179868704'");
+
+            if (length < 33)
+                _concreteFlag = new UIntConcreteBinaryFlag(length, initialValue);
+            else if (length < 65)
+                _concreteFlag = new ULongConcreteBinaryFlag(length, initialValue);
+            else
+                _concreteFlag = new UIntArrayConcreteBinaryFlag(length, initialValue);
+        }
+
+        /// <summary>
+        ///     Object Disposing
+        /// </summary>
+        public void Dispose()
+        {
+            _concreteFlag.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Set Flag Value at Given Position to True
+        /// </summary>
+        /// <param name="position">Position of Binary to Change</param>
+        public void SetFlag(ulong position)
+        {
+            _concreteFlag.SetFlag(position);
+        }
+
+        /// <summary>
+        ///     Set Flag Value at Given Position to False
+        /// </summary>
+        /// <param name="position">Position of Binary to Change</param>
+        public void ResetFlag(ulong position)
+        {
+            _concreteFlag.ResetFlag(position);
+        }
+
+        /// <summary>
+        ///     Get Multiple Binary Flag State
+        /// </summary>
+        /// <returns>True - if All Binaries are True, otherwise - False</returns>
+        public bool GetFlag()
+        {
+            return _concreteFlag.GetFlag();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Disposed)
+                return;
+
+            if (disposing)
+                Handle.Dispose();
+
+            Disposed = true;
+        }
+
+        private abstract class ConcreteBinaryFlag : IDisposable
+        {
+            protected readonly SafeHandle Handle = new SafeFileHandle(IntPtr.Zero, true);
+            protected readonly ulong Length;
+            protected bool Disposed;
+
+            protected ConcreteBinaryFlag(ulong length)
+            {
+                if (length < 2)
+                    throw new ArgumentOutOfRangeException("Length of Flag must be bigger than one");
+                Length = length;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            public virtual void SetFlag(ulong position)
+            {
+                if (position >= Length)
+                    throw new ArgumentOutOfRangeException("Position must be lesser than length");
+            }
+
+            public virtual void ResetFlag(ulong position)
+            {
+                if (position >= Length)
+                    throw new ArgumentOutOfRangeException("Position must be lesser than length");
+            }
+
+            public abstract bool GetFlag();
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (Disposed)
+                    return;
+
+                if (disposing) Handle.Dispose();
+
+                Disposed = true;
+            }
+        }
+
+        private class UIntConcreteBinaryFlag : ConcreteBinaryFlag
+        {
+            private uint _flag;
+
+            public UIntConcreteBinaryFlag(ulong length, bool initialValue) : base(length)
+            {
+                if (Length > 32)
+                    throw new ArgumentOutOfRangeException("Length of flag must be lesser than thirty-three");
+                _flag = uint.MaxValue;
+                if (initialValue)
+                    return;
+                for (uint i = 0, step = 1; i < Length; i++, step <<= 1)
+                    _flag ^= step;
+            }
+
+            public override void SetFlag(ulong position)
+            {
+                base.SetFlag(position);
+                _flag |= (uint) (1L << (int) position);
+            }
+
+            public override void ResetFlag(ulong position)
+            {
+                base.ResetFlag(position);
+                _flag &= uint.MaxValue ^ (uint) (1L << (int) position);
+            }
+
+            public override bool GetFlag()
+            {
+                return _flag == uint.MaxValue;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (Disposed)
+                    return;
+
+                if (disposing)
+                    Handle.Dispose();
+                // Free any other managed objects here.
+                //
+
+                // Free any unmanaged objects here.
+                //
+
+                Disposed = true;
+                // Call base class implementation.
+                base.Dispose(disposing);
+            }
+        }
+
+        private class ULongConcreteBinaryFlag : ConcreteBinaryFlag
+        {
+            private ulong _flag;
+
+            public ULongConcreteBinaryFlag(ulong length, bool initialValue) : base(length)
+            {
+                if (Length > 64)
+                    throw new ArgumentOutOfRangeException("Length of Flag must be lesser than sixty-five");
+                _flag = ulong.MaxValue;
+                if (initialValue)
+                    return;
+                for (ulong i = 0, step = 1; i < Length; i++, step <<= 1)
+                    _flag ^= step;
+            }
+
+            public override void SetFlag(ulong position)
+            {
+                base.SetFlag(position);
+                _flag |= (ulong) (1L << (int) position);
+            }
+
+            public override void ResetFlag(ulong position)
+            {
+                base.ResetFlag(position);
+                _flag &= ulong.MaxValue ^ (ulong) (1L << (int) position);
+            }
+
+            public override bool GetFlag()
+            {
+                return _flag == ulong.MaxValue;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (Disposed)
+                    return;
+
+                if (disposing)
+                    Handle.Dispose();
+                // Free any other managed objects here.
+                //
+
+                // Free any unmanaged objects here.
+                //
+
+                Disposed = true;
+                // Call base class implementation.
+                base.Dispose(disposing);
+            }
+        }
+
+        private class UIntArrayConcreteBinaryFlag : ConcreteBinaryFlag
+        {
+            private uint[] _flag;
+
+            public UIntArrayConcreteBinaryFlag(ulong length, bool initialValue) : base(length)
+            {
+                if (Length > 17179868704)
+                    throw new ArgumentOutOfRangeException("Length of flag must be lesser than '17179868704'");
+                _flag = new uint[(int) (Length / 32) + (Length % 32 == 0 ? 0 : 1)];
+                var change = initialValue ? 0 : Length;
+                for (var i = _flag.Length - 1; i >= 0; i--)
+                {
+                    _flag[i] = uint.MaxValue;
+                    if (initialValue)
+                        continue;
+                    if (change > 31)
+                    {
+                        _flag[i] = 0;
+                        change -= 32;
+                    }
+                    else
+                    {
+                        for (uint j = 0, step = 1; change > 0 && j < 32; j++, change--, step <<= 1)
+                            _flag[i] ^= step;
+                        change = 0;
+                    }
+                }
+            }
+
+            public override void SetFlag(ulong position)
+            {
+                base.SetFlag(position);
+                _flag[_flag.Length - 1 - (int) (position / 32)] |= (uint) (1L << (int) (position % 32));
+                //_flag |= (uint) (1L << (int) position);
+            }
+
+            public override void ResetFlag(ulong position)
+            {
+                base.ResetFlag(position);
+                _flag[_flag.Length - 1 - (int) (position / 32)] &= uint.MaxValue ^ (uint) (1L << (int) (position % 32));
+
+                //_flag &= uint.MaxValue ^ (uint) (1L << (int) position);
+            }
+
+            public override bool GetFlag()
+            {
+                return _flag.All(flagPart => flagPart == uint.MaxValue);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                _flag = null;
+
+                if (Disposed)
+                    return;
+
+                if (disposing)
+                    Handle.Dispose();
+                // Free any other managed objects here.
+                //
+
+                // Free any unmanaged objects here.
+                //
+
+                Disposed = true;
+                // Call base class implementation.
+                base.Dispose(disposing);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# IIG.BinaryFlag
+ Provides Functional for Creating and Working with Multiple Binary Flag Object
